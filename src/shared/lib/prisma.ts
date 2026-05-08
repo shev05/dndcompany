967bca0e79a50679a6f64adc1adbdd8fc725e134
Yauheni Shauchenka <yauheni.shauchenka@indev.by>
2026-05-08 15:34:06 +0300

    fix

--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/index.js';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
